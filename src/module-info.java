de4719b5b69d48fc77f55f6bf96b0b9164ea2525
sande <sande@DESKTOP-5LTDI9E>
2026-04-02 20:33:03 -0400

    yellow

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module yourMuan {
+	requires java.desktop;
 }
--- src/projectiles/NaCl.java
+++ src/projectiles/NaCl.java
@@ -1,10 +1,48 @@
 package projectiles;
-
+import java.util.Scanner;
 public class NaCl {
 
 	public static void main(String[] args) {
-		System.out.println("your mom"); 
-
+		//code starts here
+		//variables
+		Scanner sc = new Scanner(System.in);
+		int input1 = 0;
+		int input2 = 0;
+		int sum = 0;
+		
+		
+		
+		
+		System.out.println("Enter integer");
+		input1 = sc.nextInt();
+		
+		System.out.println("Enter second integer");
+		input2 = sc.nextInt();
+		
+		sum = input1 + input2;
+		System.out.println("Your answer comes out to be");
+		System.out.println(sum);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		//code ends here
 	}
 
 }
